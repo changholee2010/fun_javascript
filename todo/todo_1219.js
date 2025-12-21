6379cd85f9af119d488716aff9479524690b987f
you <you@email.com>
2025-12-22 08:59:54 +0900

    12.19.

--- todo/todo_1219.js
+++ todo/todo_1219.js
@@ -9,3 +9,26 @@
 // 4명 생성 => friends 배열에 저장.
 // 친구평균키: 175.3, 제일큰키: 184.2(홍길동)
 // 테이블태그를 활용해서 document.write(); 그리기.
+
+const friends = [
+  { name: "친구1", phone: "010-1111-1111", birth: "08-01-01", height: 167.8 },
+  { name: "친구2", phone: "010-2222-2222", birth: "08-02-01", height: 168.8 },
+  { name: "친구3", phone: "010-3333-3333", birth: "08-03-01", height: 177.8 },
+  { name: "친구4", phone: "010-4444-4444", birth: "08-04-01", height: 188.8 },
+];
+
+const avg = 0;
+let maxHeight = 0;
+let sumHeight = 0;
+let fname = "";
+
+for (let i = 0; i < friends.length; i++) {
+  if (maxHeight < friends[i].height) {
+    maxHeight = friends[i].height;
+    fname = friends[i].name;
+  }
+  sumHeight += friends[i].height;
+}
+
+avg = sumHeight / friends.length;
+console.log(`친구평균키: ${avg}, 제일큰키: ${maxHeight}(${fname})`);
