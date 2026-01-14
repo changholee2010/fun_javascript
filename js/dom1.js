a00ccdf656a89f7408ba8ee9cc2902f554b43155
funfun <fun@yedam.ac>
2026-01-15 08:52:10 +0900

    for commit

--- js/dom1.js
+++ js/dom1.js
@@ -3,13 +3,11 @@ console.log(document.querySelectorAll("div#detail>ul>li"));
 // 1.element node, 2.text node <li id="first">Apple</li>
 // 3.attribute node
 
-// element node를 만드는 메소드: createElement()
-let liTag = document.createElement("li"); // <li></li>
-// let txt = document.createTextNode("Apple"); // "Apple"
-// liTag.appendChild(txt); // <li>Apple</li>
-liTag.innerText = "Apple";
+for (let item of list) {
+  // element node를 만드는 메소드: createElement()
+  let liTag = document.createElement("li"); // <li></li>
+  liTag.innerText = item; // <li>Apple</li>
 
-console.log(liTag);
-
-// ul에 자식요소로 등록.  <ul>...<li>Apple</li></ul>
-document.querySelector("div#detail>ul").appendChild(liTag);
+  // ul에 자식요소로 등록.  <ul>...<li>Apple</li></ul>
+  document.querySelector("div#detail>ul").appendChild(liTag);
+}
