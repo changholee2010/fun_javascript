17a977b3e996f9e785873af57d911296c8ec7a58
funfun <fun@yedam.ac>
2026-01-20 11:45:34 +0900

    fff

--- js/post.js
+++ js/post.js
@@ -23,9 +23,25 @@ function makeTr(post) {
   }
   let td = document.createElement("td");
   let btn = document.createElement("button");
-  btn.innerText = "삭제";
-  btn.className = "btn btn-danger";
-  btn.setAttribute("data-no", post.id);
+  btn.innerText = "삭제"; // 텍스트.
+  btn.className = "btn btn-danger"; // 클래스 지정.
+  btn.setAttribute("data-no", post.id); // 속성추가.
+  btn.addEventListener("click", function (e) {
+    // btn의 data-no속성의 값 가져오기.
+    const no = this.dataset.no; // data-no의 속성값.
+
+    // fetch(삭제는 delete요청) 호출 start.
+    fetch("http://localhost:3000/posts/" + no, {
+      method: "delete",
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        // 화면삭제를 해야하지만 화면이 refresh되면서 목록을 새로 가져옴.
+        tr.remove();
+      })
+      .catch((err) => console.log(err));
+    // fetch(삭제는 delete요청) 호출 end.
+  });
   td.appendChild(btn);
   tr.appendChild(td);
 
