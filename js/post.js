7779f13ef9e7d33d8cfb79e49a78519200a65daa
funfun <fun@yedam.ac>
2026-01-20 11:37:07 +0900

    ff

--- js/post.js
+++ js/post.js
@@ -8,13 +8,26 @@ fetch("http://localhost:3000/posts")
   .then((data) => {
     // 데이터 건수만큼 tr 생성해주기.
     data.forEach((elem) => {
-      let tr = document.createElement("tr");
-      for (let field of fields) {
-        let td = document.createElement("td");
-        td.innerText = elem[field];
-        tr.appendChild(td);
-      }
-      target.appendChild(tr);
+      target.appendChild(makeTr(elem));
     });
   })
   .catch((err) => console.log(err));
+
+// tr생성함수.
+function makeTr(post) {
+  let tr = document.createElement("tr");
+  for (let field of fields) {
+    let td = document.createElement("td");
+    td.innerText = post[field];
+    tr.appendChild(td);
+  }
+  let td = document.createElement("td");
+  let btn = document.createElement("button");
+  btn.innerText = "삭제";
+  btn.className = "btn btn-danger";
+  btn.setAttribute("data-no", post.id);
+  td.appendChild(btn);
+  tr.appendChild(td);
+
+  return tr;
+}
