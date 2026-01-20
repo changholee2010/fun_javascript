f623cbdd90cdb649ca24b58b03a835c168302cd7
funfun <fun@yedam.ac>
2026-01-20 12:39:50 +0900

    ffff

--- js/post.js
+++ js/post.js
@@ -2,23 +2,50 @@
 // 1. fetch 글목록 출력.
 const fields = ["id", "title", "author"];
 const target = document.querySelector("#postList");
+let new_id = 0; // 신규글번호
 
-fetch("http://localhost:3000/posts")
+fetch("http://192.168.0.29:3000/posts")
   .then((resp) => resp.json())
   .then((data) => {
     // 데이터 건수만큼 tr 생성해주기.
     data.forEach((elem) => {
       target.appendChild(makeTr(elem));
     });
+    // reduce.
+    new_id = data.reduce((acc, elem) => {
+      return acc > elem.id ? acc : elem.id;
+    }, 0);
+    console.log(new_id);
   })
   .catch((err) => console.log(err));
+
+// 등록이벤트.
+document
+  .querySelector('form[name="addForm"]')
+  .addEventListener("submit", (e) => {
+    e.preventDefault();
+    // title, author => 입력값.
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    // fetch. Post요청처리.
+    fetch("http://localhost:3000/posts", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: Number(new_id) + 1, title, author }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  });
 
 // tr생성함수.
 function makeTr(post) {
   let tr = document.createElement("tr");
   for (let field of fields) {
     let td = document.createElement("td");
-    td.innerText = post[field];
+    td.innerText = post[field]; // 객체[속성] => 값을 가져옴.
     tr.appendChild(td);
   }
   let td = document.createElement("td");
@@ -31,7 +58,7 @@ function makeTr(post) {
     const no = this.dataset.no; // data-no의 속성값.
 
     // fetch(삭제는 delete요청) 호출 start.
-    fetch("http://localhost:3000/posts/" + no, {
+    fetch("http://192.168.0.29:3000/posts/" + no, {
       method: "delete",
     })
       .then((resp) => resp.json())
@@ -41,9 +68,10 @@ function makeTr(post) {
       })
       .catch((err) => console.log(err));
     // fetch(삭제는 delete요청) 호출 end.
-  });
+  }); // end of btn.addEventListener.
+
   td.appendChild(btn);
   tr.appendChild(td);
 
   return tr;
-}
+} // end of makeTr.
